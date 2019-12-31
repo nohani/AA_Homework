e579b2123bd63a5db42f3b4c22ae52f99da837e5
nohani <nicole.takuhi@gmail.com>
2019-12-31 12:02:50 -0500

    Version 1 of Mancala

---
+++ W6D2/mancala/lib/board.rb
@@ -0,0 +1,62 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @cups = Array.new(14){[]}
+    self.place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+    self.cups.each_with_index do |cup, i|
+      if i != 6 && i != 13
+        4.times {cup << :stone}
+      end
+    end
+
+    self.cups
+
+  end
+
+  def valid_move?(start_pos)
+    if !(0..5).include?(start_pos) && !(7..12).include?(start_pos)
+        raise "Invalid starting cup"
+    elsif self.cups[start_pos].empty?
+        raise "Starting cup is empty"
+    end   
+  end
+
+  def make_move(start_pos, current_player_name)
+    hand = self.cups[start_pos] 
+    self.cups[start_pos] == []
+
+      i = (start_pos + 1) % self.cups.length
+      while i < self.cups.length
+        if !hand.empty? 
+        self.cups[i % self.cups.length] << hand.shift
+        end
+      i += 1
+      end
+
+      self.render
+    end
+
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+  end
+
+  def winner
+  end
+end
